e7feb0c6020c49d5e91e9921de3f4a5c77b79c73
lilu <lilu@bigdatacd.com>
2025-11-19 10:54:15 +0800

    重构 inject.js，优化模块导入和全局函数挂载逻辑，增强代码可读性和稳定性

--- inject.js
+++ inject.js
@@ -1,49 +1,63 @@
-
-
-webpackChunkchat_group_vue.push([
-    [99999],
-    {},
-    (r) => { window.wr = r }
-]);
+Object.keys(window).forEach((key) => {
+    if (key.startsWith("webpackChunk")) {
+        console.log(key);
+        window[key].push([
+            [99999],
+            {},
+            (r) => {
+                window.wr = r;
+            },
+        ]);
+    }
+});
 // 用来挂全局的函数
 function exposeLibs(runtime) {
+    if (!runtime) return;
     const exportsMap = {};
-
     for (const id in runtime.m) {
-        const moduleFactory = runtime.m[id];
-        if (typeof moduleFactory === 'function') {
-            const exp = {};
-            try {
-                moduleFactory({ exports: exp }, exp, runtime);
-            } catch (e) {
-                // 某些模块执行可能报错，忽略
-            }
-            const candidate = exp.default || exp;
+        let exp = runtime(id);
 
+        const candidate = exp.default || exp;
 
-            if (candidate && candidate.version) {
-                console.log("candidate", exp)
-            }
-
-
-            // 判断 axios
-            if (!exportsMap.axios && candidate && candidate.get && candidate.post && candidate.defaults) {
-                exportsMap.axios = candidate;
-            }
-
-            // 判断 Vue
-            if (!exportsMap.Vue && candidate && candidate.version && candidate.component && candidate.directive) {
-                exportsMap.Vue = candidate;
-            }
-
-            // 判断 ElementUI
-            if (!exportsMap.ElementUI && candidate && candidate.install && candidate.version && candidate.Button) {
-                exportsMap.ElementUI = candidate;
-            }
-
-            // 提前结束判断
-            if (exportsMap.axios && exportsMap.Vue && exportsMap.ElementUI) break;
+        if (candidate && candidate.version) {
+            console.log("candidate", id, exp);
         }
+
+        // 判断 axios
+        if (
+            !exportsMap.axios &&
+            candidate &&
+            candidate.get &&
+            candidate.post &&
+            candidate.defaults
+        ) {
+            exportsMap.axios = candidate;
+        }
+
+        // 判断 Vue
+        if (
+            !exportsMap.Vue &&
+            candidate &&
+            candidate.version &&
+            candidate.component &&
+            candidate.directive
+        ) {
+            exportsMap.Vue = candidate;
+        }
+
+        // 判断 ElementUI
+        if (
+            !exportsMap.ElementUI &&
+            candidate &&
+            candidate.install &&
+            candidate.version &&
+            candidate.Button
+        ) {
+            exportsMap.ElementUI = candidate;
+        }
+
+        // 提前结束判断
+        if (exportsMap.axios && exportsMap.Vue && exportsMap.ElementUI) break;
     }
 
     // 挂到全局
@@ -56,103 +70,38 @@ function exposeLibs(runtime) {
 }
 
 // 执行
-exposeLibs(wr);
-
-
-
-
-
-
-
-
-
+exposeLibs(window.wr);
 
 const delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
 
 async function getCourseDetails(param) {
     const res = await axios({
-        url: '/studyCourse/getCourseDetails?courseId=' + param,
-        method: 'get'
+        url: "/studyCourse/getCourseDetails?courseId=" + param,
+        method: "get",
     });
     return res.returnData;
 }
-
-
-
-// 获取进程
-async function findCourseStudyDetail(param) {
-    const res = await axios({
-        url: '/studyCourseUser/process?courseId=' + param,
-        method: 'get'
-    });
-    return res.returnData;
-}
-
 
 async function recordStudyProcess(param) {
     const res = await axios({
-        url: '/studyCourseUser/recordProcess',
-        method: 'post',
-        data: param
+        url: "/studyCourseUser/recordProcess",
+        method: "post",
+        data: param,
     });
-
-    // if (res.returnData != 1) {
-    //     console.log("param", param)
-    // }
-
     return res.returnData;
 }
-
-
-async function refreshProcess(param) {
-    const res = await axios({
-        url: '/studyCourseUser/refreshProcess',
-        method: 'post',
-        data: param
-    });
-
-    return res.returnData;
-}
-
-
-async function getSubsectionDetail(param) {
-    const res = await axios({
-        url: '/studySubsection/getSubsectionDetail',
-        method: 'post',
-        data: param
-    });
-
-    return res.returnData;
-}
-
-
-
-// recordStudyProcess({ 
-//     "courseId": "1983474287572594688", //课程ID
-//     "chapterId": "1983474473980047360", //章节ID
-//     "subsectionId": "1983475172650430464", // 子节点ID
-//     "studyTime": 500,   //观看时间
-//     "state": "2"  // 当前状态
-// })
-
-
-
 if (window.Vue) {
-
     // debugger
 
     const div = document.createElement("div");
     document.body.appendChild(div);
 
-
-    console.log("div")
+    console.log("div");
     const app = new Vue({
         el: div,
         data() {
             return {
-                title: "自动学习中...请保持窗口不关闭",
-                gridData: [],
-                dialogTableVisible: false,
+                isStart: false,
                 style: {
                     position: "fixed",
                     top: "109px",
@@ -174,103 +123,106 @@ if (window.Vue) {
         // `,
         render(h) {
             return this.show
-                ? h(
-                    'div',
-                    { style: this.style },
-                    [
-                        h(
-                            'button',
-                            {
-                                attrs: { type: 'primary' },
-                                on: {
-                                    click: async () => {
-                                        while (true) {
-                                            try {
-                                                await this.handleClick()
-                                                break;
-                                            } catch (error) {
-                                                await delay(3000);
-                                                console.log("error", error)
-                                            }
-                                        }
-                                    }
-                                }
-                            },
-                            '一键学习'
-                        )
-                    ]
-                )
+                ? h("div", { style: this.style }, [
+                      h(
+                          "el-button",
+                          {
+                              attrs: { type: "primary", loading: this.isStart },
+                              on: {
+                                  click: async () => {
+                                      while (true) {
+                                          try {
+                                              await this.handleClick();
+                                              break;
+                                          } catch (error) {
+                                              await delay(3000);
+                                              console.log("error", error);
+                                          }
+                                      }
+                                  },
+                              },
+                          },
+                          this.isStart ? "学习中..." : "一键学习"
+                      ),
+                  ])
                 : null;
         },
         methods: {
             async handleClick() {
-                const query = new URLSearchParams(window.location.search)
-                console.log("query", query.get("courseId"))
-                const courseId = query.get("courseId")
+                if (this.isStart) {
+                    return;
+                }
+                const query = new URLSearchParams(window.location.search);
+                console.log("query", query.get("courseId"));
+                const courseId = query.get("courseId");
                 if (!courseId) {
-                    alert("未获取到课程id，请点击某个课程进入课程培训列表页！")
+                    this.$alert(
+                        "未获取到课程id，请点击某个课程进入课程培训详情页！"
+                    );
+                    return;
                 }
 
-                const courseInfo = await getCourseDetails(courseId)
-
-                console.log("课程信息", courseInfo)
-
-                const chapters = courseInfo.chapters
-
-                console.log("获取章节", chapters)
-
-                
-
-                for (const [chapterIndex,chapter] of chapters.entries()) {
-                    for (const [sectionIndex,section] of chapter.studySubsections.entries()) {
-                        const { courseId, chapterId, id: subsectionId } = section
-                        const { state, videoTime } = section.studyFiles
-
-                        console.log("遍历每个章节：", courseId, chapterId, subsectionId, videoTime)
-
-                        document.querySelector(".courseIntro").innerText = `正在学习：${chapterIndex + 1}-${sectionIndex + 1}`
-
-                        if(state != '3'){
-                            // await refreshProcess({
-                            //     "courseId": courseId, //课程ID
-                            //     "chapterId": chapterId, //章节ID
-                            //     "subsectionId": subsectionId, // 子节点ID
-                            //     "studyTime": 0,   //观看时间
-                            // })
-    
-                            let recordValue = 2
-                            while (recordValue != 1) {
-                                await delay(1000 * 45)
-                                recordValue = await recordStudyProcess({
-                                    "courseId": courseId, //课程ID
-                                    "chapterId": chapterId, //章节ID
-                                    "subsectionId": subsectionId, // 子节点ID
-                                    "studyTime": parseInt(videoTime),   //观看时间 parseInt(videoTime) + 1
-                                    "state": "2"  // 当前状态
-                                })
-                            }
-                            console.log("recordValue",recordValue)
-                        }
-                        console.log("该章节学习完毕")
+                this.isStart = true;
+                while (true) {
+                    try {
+                        await this.study(courseId);
+                        break;
+                    } catch (error) {
+                        await delay(3000);
+                        console.log("error", error);
                     }
                 }
-                window.location.reload()
+            },
+            async study(courseId) {
+                const loading = this.$loading({
+                    target: document.querySelector(".courseTxt"),
+                    lock: true,
+                    text: `正在自动学习...`,
+                    // background: "rgba(0, 0, 0, 0.7)",
+                });
+                const courseInfo = await getCourseDetails(courseId);
+                const chapters = courseInfo.chapters;
+                for (const [chapterIndex, chapter] of chapters.entries()) {
+                    for (const [
+                        sectionIndex,
+                        section,
+                    ] of chapter.studySubsections.entries()) {
+                        const {
+                            courseId,
+                            chapterId,
+                            id: subsectionId,
+                        } = section;
+                        const { state, videoTime } = section.studyFiles;
+
+                        // console.log(
+                        //     "遍历每个章节：",
+                        //     courseId,
+                        //     chapterId,
+                        //     subsectionId,
+                        //     videoTime
+                        // );
+                        loading.text = `正在自动学习：${chapterIndex + 1}-${
+                            sectionIndex + 1
+                        }`;
+                        if (state != "3") {
+                            let recordValue = 0;
+                            while (recordValue != 1) {
+                                await delay(1000 * 45);
+                                recordValue = await recordStudyProcess({
+                                    courseId: courseId, //课程ID
+                                    chapterId: chapterId, //章节ID
+                                    subsectionId: subsectionId, // 子节点ID
+                                    studyTime: parseInt(videoTime), //观看时间 parseInt(videoTime) + 1
+                                    state: "2", // 当前状态
+                                });
+                            }
+                        }
+                    }
+                }
+                window.location.reload();
             },
         },
     });
 
-    window.app = app
-
-    function show() {
-        if (location.hash.includes("onlineLearn/myLearn")) {
-            app.show = true;
-        } else {
-            app.show = true;
-        }
-    }
-    show();
-    setInterval(() => {
-        show();
-    }, 1000);
+    window.app = app;
 }
-
